e64c9f2d82db2148b1b57a7e60147faf3fd6b59e
thomasnicholson <40121607+thomasnicholson@users.noreply.github.com>
2020-09-25 22:34:17 +0200

    reorganisation

---
+++ festival2020/script.js
@@ -0,0 +1,8 @@
+function hamburger_click() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
