95347b006be910fe9e58277de77f9ba1c3f9eb25
thomasnicholson <40121607+thomasnicholson@users.noreply.github.com>
2020-09-26 10:09:35 +0200

    organisation

--- css-js/script.js
+++ css-js/script.js
@@ -1,3 +1,4 @@
+// Hamburger menu functionality
 function hamburger_click() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
--- festival2020/script.js
+++
@@ -1,8 +0,0 @@
-function hamburger_click() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
