19f497c2492993e15c771cc89e8ff404bc77bcd2
thomasnicholson <40121607+thomasnicholson@users.noreply.github.com>
2020-09-26 10:45:46 +0200

    quick js fix

--- css-js/script.js
+++ css-js/script.js
@@ -1,7 +1,7 @@
 // Hamburger menu functionality
 function hamburger_click() {
   var menu = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+  if (menu.style.display === "block") {
     menu.style.display = "none";
   } else {
     menu.style.display = "block";
